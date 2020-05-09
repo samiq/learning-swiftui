5209c386acba81ca75c72cebeb443d59395f3c2f
Gilbert Corrales <gcorrales@gmail.com>
2020-05-09 16:17:36 +0100

    making components work

--- 02. Lessons.playground/Pages/03. Components .xcplaygroundpage/Contents.swift
+++ 02. Lessons.playground/Pages/03. Components .xcplaygroundpage/Contents.swift
@@ -3,8 +3,9 @@ import SwiftUI
 import PlaygroundSupport
 
 struct ContentView : View {
+    
     var body : some View {
-        ZStack { 
+        ZStack {
             // Background Card
             VStack {
                 Spacer()
@@ -16,33 +17,7 @@ struct ContentView : View {
             .offset(x:0, y:-20)
             
             // Card
-            VStack{
-                HStack{
-                    VStack(alignment: .leading){
-                        Text("UI Design")
-                            .font(.title)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                        Text("Certificate")
-                            .foregroundColor(Colors.accent)
-                    }
-                    Spacer()
-                    Image(uiImage:#imageLiteral(resourceName: "background.png"))
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 20)
-                Spacer()
-                Image(uiImage:#imageLiteral(resourceName: "Card1.png"))
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 300, height:110, alignment: .top)
-            }
-            .frame(width:340, height: 220)
-            .background(Color.black)
-            .cornerRadius(20)
-            .shadow(radius: 20)
+            CardView()
         }
     }
 }
